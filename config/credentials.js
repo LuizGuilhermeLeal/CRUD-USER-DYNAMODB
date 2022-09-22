eb45ce95a29f50f5fad4438b83698f3215eb1c64
LuizGuilhermeLeal <guileeal@live.com>
2022-09-22 16:35:12 -0300

    last commit

---
+++ config/credentials.js
@@ -0,0 +1,8 @@
+module.exports = {
+    AwsConfig: {
+        'region': 'sa-east-1',
+        'endpoint': 'http://dynamodb.sa-east-1.amazonaws.com',
+        'accessKeyId':'',
+        'secretAccessKey': '',
+    }
+}
